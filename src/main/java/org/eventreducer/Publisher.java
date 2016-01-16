d8ced44bcd44b2d174dd71637fb4d3184d2e74d5
Yurii Rashkovskii <yrashk@gmail.com>
2016-01-15 21:38:23 -0800

    Implement convenience Publisher API helpers

--- src/main/java/org/eventreducer/Publisher.java
+++ src/main/java/org/eventreducer/Publisher.java
@@ -8,4 +8,17 @@ import java.util.function.Consumer;
 public interface Publisher {
 
     void publish(Command command, BiConsumer<Optional, List<Event>> completionHandler, Consumer<Throwable> exceptionHandler);
+
+    default void publish(Command command, BiConsumer<Optional, List<Event>> completionHandler) {
+        publish(command, completionHandler, throwable -> {});
+    }
+
+    default void publish(Command command, Consumer<Throwable> exceptionHandler) {
+        publish(command,  (optional, events) -> {}, exceptionHandler);
+    }
+
+    default void publish(Command command) {
+        publish(command, (optional, events) -> {});
+    }
+
 }
