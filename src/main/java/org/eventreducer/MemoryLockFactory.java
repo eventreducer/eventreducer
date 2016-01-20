6ca6a6a37332c7754bd209ddf0c0ad06741d3eaf
Yurii Rashkovskii <yrashk@gmail.com>
2016-01-20 11:29:26 -0800

    Fix MemoryLock implementation

--- src/main/java/org/eventreducer/MemoryLockFactory.java
+++ src/main/java/org/eventreducer/MemoryLockFactory.java
@@ -2,21 +2,24 @@ package org.eventreducer;
 
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Semaphore;
 
 public class MemoryLockFactory extends LockFactory {
 
+    private Map<Object, Semaphore> locks = new HashMap<>();
+
     @Override
     public org.eventreducer.Lock lock(Object lock) {
-        Semaphore semaphore = new Semaphore(1);
+        Semaphore semaphore = locks.containsKey(lock) ? locks.get(lock) : new Semaphore(1);
+        locks.put(lock, semaphore);
         semaphore.acquireUninterruptibly();
         return new MemoryLock(semaphore);
     }
 
     static class MemoryLock implements Lock {
         private final Semaphore semaphore;
-        @Getter
-        private boolean locked = true;
 
         public MemoryLock(Semaphore semaphore) {
             this.semaphore = semaphore;
@@ -24,8 +27,12 @@ public class MemoryLockFactory extends LockFactory {
 
         @Override
         public void unlock() {
-            locked = false;
             semaphore.release();
+        }
+
+        @Override
+        public boolean isLocked() {
+            return semaphore.availablePermits() == 0;
         }
 
     }
