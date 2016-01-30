e98fbdb7078cf663d4cd3a7b5818940464a0a3e4
Yurii Rashkovskii <yrashk@gmail.com>
2016-01-30 13:26:41 -0800

    Make ComparableTimeStamp comparable to itself

--- src/main/java/org/eventreducer/ComparableTimeStamp.java
+++ src/main/java/org/eventreducer/ComparableTimeStamp.java
@@ -3,14 +3,16 @@ package org.eventreducer;
 import org.apache.commons.net.ntp.TimeStamp;
 import org.eventreducer.hlc.HybridTimestamp;
 
-public class ComparableTimeStamp extends TimeStamp {
+public class ComparableTimeStamp implements Comparable<ComparableTimeStamp> {
+
+    private final TimeStamp timeStamp;
 
     public ComparableTimeStamp(TimeStamp timeStamp) {
-        super(timeStamp.toString());
+        this.timeStamp = timeStamp;
     }
 
     @Override
-    public int compareTo(TimeStamp anotherTimeStamp) {
-        return HybridTimestamp.compare(this, anotherTimeStamp);
+    public int compareTo(ComparableTimeStamp anotherTimeStamp) {
+        return HybridTimestamp.compare(this.timeStamp, anotherTimeStamp.timeStamp);
     }
 }
